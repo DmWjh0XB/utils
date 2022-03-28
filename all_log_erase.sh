54da6138fdc6db107d1609702727201da9015a69
DmWjh0XB <62544296+DmWjh0XB@users.noreply.github.com>
2022-03-28 12:43:56 +0200

    Update all_log_erase.sh

--- all_log_erase.sh
+++ all_log_erase.sh
@@ -1,4 +1,5 @@
-#! /bin/sh
-find /var/log -type f -delete
-rm $HISTFILE
-cat /dev/null > $HISTFILE
+#!/bin/sh
+sudo journalctl --vacuum-size=0
+sudo find /var/log -type f -delete
+sudo rm -f $HISTFILE
+sudo cat /dev/null > $HISTFILE
